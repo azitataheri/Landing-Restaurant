c87f43aebe6636a969473b169f6523a9e52c7fda
Azita Taheri <96353949+azitataheri@users.noreply.github.com>
2022-07-22 13:25:29 +0430

    Add the menu tabs section

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,38 @@
+//---------------- VARIABALES -------------------- //
 const primaryNav = document.querySelector('.primary-navigation')
 const mobileNavToggle = document.querySelector('.mobile-nav-toggle')
+const navTab = document.getElementById('nav-tab');
 
 
-    mobileNavToggle.addEventListener('click', () => {
-        const visibilityNan = primaryNav.getAttribute('data-visible')
-        if( visibilityNan === "false"){
-            primaryNav.setAttribute("data-visible", true)
-            mobileNavToggle.setAttribute("aria-expanded", true)
-        }else if( visibilityNan === "true"){
-            primaryNav.setAttribute("data-visible", false)
-            mobileNavToggle.setAttribute("aria-expanded", false)
-        }
-    })
 
+
+//---------------- NAVIGATION -------------------- //
+mobileNavToggle.addEventListener('click', () => {
+    const visibilityNan = primaryNav.getAttribute('data-visible')
+    if( visibilityNan === "false"){
+        primaryNav.setAttribute("data-visible", true)
+        mobileNavToggle.setAttribute("aria-expanded", true)
+    }else if( visibilityNan === "true"){
+        primaryNav.setAttribute("data-visible", false)
+        mobileNavToggle.setAttribute("aria-expanded", false)
+    }
+})
+
+
+//------------------- TABS ------------------------//
+function onTabClick(event) {
+let activeTabs = document.querySelectorAll('.active');
+activeTabs.forEach(function(tab) {
+    tab.className = tab.className.replace('active', '');
+});
+
+// activate new tab and panel
+event.target.parentElement.className += ' active';
+document.getElementById(event.target.href.split('#')[1]).className += ' active';
+}
+
+// click on tab and active or deactive it
+navTab.addEventListener('click', onTabClick, false);
+
+      
+      
